53b5a05e8f6bd3007153c2cde40522a57d70b322
unknown <vladancupric@gmail.com>
2022-10-04 18:08:45 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-
-import './App.css';
-import Home from './components/HomePage/Home';
-import React, {Component} from 'react';
-import {BrowserRouter as router, route, Link} from "react-router-dom"
-
+import "./App.css";
+import Home from "./components/HomePage/Home";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-
-  
   return (
-   <>
-   <h1>Welcome to the Home Page</h1>
-  
+    <>
+      <h1>Welcome to the Home Page</h1>
+      <Router>
+        <div>
+          <Route exact path="/" component={<NewBracketContainer />} />
+          <Route exact path="/add-teams" component={<AddsTeamtContainer />} />
+          <Route exact path="/match-view" component={<MatchViewContainer />} />
+          <Route exact path="/game-won" component={<WinnerContainer />} />
+        </div>
+      </Router>
     </>
   );
 }
